eb9c2e5ea8c7691a14ef981f33ea81f60cddfd30
Alex22Simi <159585628+Alex22Simi@users.noreply.github.com>
2024-10-28 19:16:18 +0000

    cart

--- index.js
+++ index.js
@@ -13,23 +13,32 @@ async function displayProducts(){
 
     productContainer.innerHTML = products.map(
        mapProductToCard).join("");
-       const addToCartButtons = document.querySelectorAll(".add-to-chart");
+       const addToCartButtons = document.querySelectorAll(".add-to-cart");
        console.log(addToCartButtons);
        addToCartButtons.forEach((button)=>{
          button.addEventListener("click",() => {
             
           const productId = button.getAttribute("data-productId");
+          const name = button.getAttribute("data-name");
+          const price = button.getAttribute("data-price");
+          const imageURL = button.getAttribute("data-image");
+
+
           let cart = JSON.parse(localStorage.getItem("cart")) || {};
 
           if(cart[productId])
           {
-            cart[productid].quantity++;
+            cart[productId].quantity++;
           }else{
-            cart[productId] = {quantity: 1};
+            cart[productId] = {quantity: 1,
+               price: Number(price),
+               image: imageURL,
+               name: name,
+            };
           }
 
           localStorage.setItem("cart",JSON.stringify(cart));
-          
+
     
     });
 
